381df61a35a49d1ce7557a6c52cfe7a2c48ee919
Richard <richard.yang.tong@gmail.com>
2021-07-19 20:37:35 -0400

    Added notes.

--- JVMDemo/chapter01/src/com/dragontalker/java/StackStruTest.java
+++ JVMDemo/chapter01/src/com/dragontalker/java/StackStruTest.java
@@ -1,4 +1,9 @@
 package com.dragontalker.java;
 
 public class StackStruTest {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
 }
