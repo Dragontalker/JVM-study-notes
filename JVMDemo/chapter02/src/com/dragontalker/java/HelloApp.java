99909060ecd67c20a52978caa270634fb62a7611
Richard <richard.yang.tong@gmail.com>
2021-07-20 10:48:35 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java/HelloApp.java
+++ JVMDemo/chapter02/src/com/dragontalker/java/HelloApp.java
@@ -1,4 +1,12 @@
 package com.dragontalker.java;
 
 public class HelloApp {
+
+    // 在prepare环节, a只会被赋值为0
+    // 在initial环节, a才会被初始化为1
+    private static int a = 1;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
 }
