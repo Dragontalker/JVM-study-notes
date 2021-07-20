c0d960c31168fe1ad3ef4859ab9b541e69631a3f
Richard <richard.yang.tong@gmail.com>
2021-07-20 10:49:53 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java/HelloApp.java
+++ JVMDemo/chapter02/src/com/dragontalker/java/HelloApp.java
@@ -6,7 +6,11 @@ public class HelloApp {
     // 在initial环节, a才会被初始化为1
     private static int a = 1;
 
+    // 如果是final修饰的常量, 则在prepare环节就会被赋值
+    private final static int b = 2;
+
     public static void main(String[] args) {
         System.out.println(a);
+        System.out.println(b);
     }
 }
