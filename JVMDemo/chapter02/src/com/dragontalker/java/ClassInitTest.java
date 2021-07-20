99f4107599f609303edd25a513694dcf3c3513ad
Richard <richard.yang.tong@gmail.com>
2021-07-20 10:55:12 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java/ClassInitTest.java
+++ JVMDemo/chapter02/src/com/dragontalker/java/ClassInitTest.java
@@ -1,4 +1,9 @@
 package com.dragontalker.java;
 
 public class ClassInitTest {
+    private static int num = 1;
+
+    public static void main(String[] args) {
+        System.out.println(ClassInitTest.num);
+    }
 }
