46cee295a43b4c9bdc0be36a52fd6bd74b225ae9
Richard <richard.yang.tong@gmail.com>
2021-07-19 21:07:09 -0400

    Added notes.

--- JVMDemo/chapter01/src/com/dragontalker/java/StackStruTest.java
+++ JVMDemo/chapter01/src/com/dragontalker/java/StackStruTest.java
@@ -1,9 +1,13 @@
 package com.dragontalker.java;
 
 public class StackStruTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int i = 2;
         int j = 3;
         int k = i + j;
+
+        Thread.sleep(6000);
+
+        System.out.println("hello");
     }
 }
