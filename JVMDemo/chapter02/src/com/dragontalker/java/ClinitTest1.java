7e3b08775572ae37c35d497b516017d1fe226747
Richard <richard.yang.tong@gmail.com>
2021-07-20 11:11:06 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java/ClinitTest1.java
+++ JVMDemo/chapter02/src/com/dragontalker/java/ClinitTest1.java
@@ -1,4 +1,18 @@
 package com.dragontalker.java;
 
 public class ClinitTest1 {
+    static class Father {
+        public static int A = 1;
+        static {
+            A = 2;
+        }
+    }
+
+    static class Son extends Father {
+        public static int B = A;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Son.B);
+    }
 }
