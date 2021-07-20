aaa20a23edef6ecb38045fade0e5f5ba329cc3f0
Richard <richard.yang.tong@gmail.com>
2021-07-20 10:35:36 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java/HelloLoader.java
+++ JVMDemo/chapter02/src/com/dragontalker/java/HelloLoader.java
@@ -1,4 +1,9 @@
 package com.dragontalker.java;
 
 public class HelloLoader {
+
+    public static void main(String[] args) {
+        System.out.println("谢谢ClassLoader加载我....");
+        System.out.println("你的大恩大德, 我下辈子再报!");
+    }
 }
