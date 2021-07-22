1bb8d4259d2b7945d108374726b1166679b18c23
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:52:55 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/StringTest.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/StringTest.java
@@ -1,4 +1,10 @@
 package com.dragontalker.java1;
 
 public class StringTest {
+
+    public static void main(String[] args) {
+
+        String str = new String();
+        System.out.println("Hello, world!");
+    }
 }
