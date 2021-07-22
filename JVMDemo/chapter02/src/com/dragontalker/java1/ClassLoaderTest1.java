ed3dcba09e54234ef3769aa70b3e1e28656440f8
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:24:35 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
@@ -1,4 +1,13 @@
 package com.dragontalker.java1;
 
+import java.net.URL;
+
 public class ClassLoaderTest1 {
+    public static void main(String[] args) {
+        System.out.println("***********启动类加载器***********");
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for (URL element : urls) {
+            System.out.println(element.toExternalForm());
+        }
+    }
 }
