2d2f7f84c4b59c05ce08f3fb039edfe4b8b87940
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:09:38 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest.java
@@ -1,4 +1,10 @@
 package com.dragontalker.java1;
 
 public class ClassLoaderTest {
+    public static void main(String[] args) {
+
+        // 获取系统类加载器
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(systemClassLoader); // sun.misc.Launcher$AppClassLoader@18b4aac2
+    }
 }
