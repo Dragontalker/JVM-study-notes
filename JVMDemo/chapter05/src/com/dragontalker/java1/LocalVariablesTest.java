592f66be7c7eb8b3289c050eba8dc096c3042a35
Richard <richard.yang.tong@gmail.com>
2021-07-22 17:20:06 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java1/LocalVariablesTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java1/LocalVariablesTest.java
@@ -1,4 +1,40 @@
 package com.dragontalker.java1;
 
+import java.util.Date;
+
 public class LocalVariablesTest {
+    private int count = 0;
+
+    public static void main(String[] args) {
+        LocalVariablesTest test = new LocalVariablesTest();
+        int num = 10;
+        test.test1();
+    }
+
+    public void test1() {
+        Date date = new Date();
+        String name1 = "dragontalker.com";
+        String info = test2(date, name1);
+        System.out.println(date + name1);
+    }
+
+    public String test2(Date dateP, String name2) {
+        dateP = null;
+        name2 = "richardyang";
+        double weight = 200.05;
+        char gender = '男';
+        return dateP + name2;
+    }
+
+    public void test3() {
+        count++;
+    }
+
+    public void test4() {
+        int a = 0;
+        {
+            int b = 0;
+            b = a + 1;
+        }
+    }
 }
