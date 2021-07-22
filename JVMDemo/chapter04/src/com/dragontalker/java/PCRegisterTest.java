37145c1ba097d91a184e135595d67595506e3bc3
Richard <richard.yang.tong@gmail.com>
2021-07-22 16:11:14 -0400

    Added notes.

--- JVMDemo/chapter04/src/com/dragontalker/java/PCRegisterTest.java
+++ JVMDemo/chapter04/src/com/dragontalker/java/PCRegisterTest.java
@@ -6,5 +6,9 @@ public class PCRegisterTest {
         int i = 10;
         int j = 20;
         int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
     }
 }
