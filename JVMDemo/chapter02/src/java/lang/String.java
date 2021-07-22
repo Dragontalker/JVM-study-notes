893f242f4926f9586d9f712f7e969e110cfb1700
Richard <richard.yang.tong@gmail.com>
2021-07-22 10:05:24 -0400

    Added notes.

--- JVMDemo/chapter02/src/java/lang/String.java
+++ JVMDemo/chapter02/src/java/lang/String.java
@@ -8,6 +8,7 @@ public class String {
 
     // Error: Main method not found in class java.lang.String
     // 因为双亲委派机制, 会层层上传到引导类加载器, 而引导类加载器在原有java.lang.String中找不到main方法
+    // 我们自定义的String类根本没有被加载
     public static void main(String[] args) {
         System.out.println("Hello, string!");
     }
