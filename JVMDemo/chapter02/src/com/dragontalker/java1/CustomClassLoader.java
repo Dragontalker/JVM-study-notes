6ba47f109d41cf9a8580e10b8a6ae5184e6b631d
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:42:41 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/CustomClassLoader.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/CustomClassLoader.java
@@ -23,7 +23,7 @@ public class CustomClassLoader extends ClassLoader{
 
     private byte[] getClassFromCustomPath(String name) {
         // 从自定义路径中加载指定类: 细节省略
-
+        // 如果指定路径的字节码文件进行了加密, 则需要在此方法中进行解密操作
         return null;
     }
 }
