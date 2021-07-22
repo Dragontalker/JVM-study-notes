c8669dc4d6073c027739dc97b166728b6e7628ba
Richard <richard.yang.tong@gmail.com>
2021-07-22 16:47:16 -0400

    Added notes.

---
+++ JVMDemo/chapter05/src/com/dragontalker/java1/StackFrameTest.java
@@ -0,0 +1,29 @@
+package com.dragontalker.java1;
+
+public class StackFrameTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void method1() {
+        System.out.println("method1()开始执行...");
+        method2();
+        System.out.println("method1()执行结束...");
+    }
+
+    public int method2() {
+        System.out.println("method2()开始执行...");
+        int i = 10;
+        method3();
+        System.out.println("method2()即将结束...");
+        return i;
+    }
+
+    public double method3() {
+        System.out.println("method3()开始执行...");
+        double j = 20.0;
+        System.out.println("method3()即将结束...");
+        return j;
+    }
+}
