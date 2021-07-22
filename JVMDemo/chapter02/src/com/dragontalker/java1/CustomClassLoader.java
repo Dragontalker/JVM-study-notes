6238108ea85d49215f40b7bbfb4dc2eeb83ce063
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:41:42 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/CustomClassLoader.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/CustomClassLoader.java
@@ -1,4 +1,27 @@
 package com.dragontalker.java1;
 
-public class CustomClassLoader {
+import java.io.FileNotFoundException;
+
+public class CustomClassLoader extends ClassLoader{
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        try {
+            byte[] result = getClassFromCustomPath(name);
+            if (result == null) {
+                throw new FileNotFoundException();
+            } else {
+                return defineClass(name, result, 0, result.length);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        throw new ClassNotFoundException();
+    }
+
+    private byte[] getClassFromCustomPath(String name) {
+        return null;
+    }
 }
