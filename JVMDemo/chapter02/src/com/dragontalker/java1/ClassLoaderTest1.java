eca21f71e730782facc97721c9341c961059f8de
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:26:38 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
@@ -5,6 +5,7 @@ import java.net.URL;
 public class ClassLoaderTest1 {
     public static void main(String[] args) {
         System.out.println("***********启动类加载器***********");
+        // 获取BootstrapClassLoader能够加载的api的路径
         URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
         for (URL element : urls) {
             System.out.println(element.toExternalForm());
