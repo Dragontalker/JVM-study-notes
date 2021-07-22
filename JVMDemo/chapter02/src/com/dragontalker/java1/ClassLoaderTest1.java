4bbc2155ff89d78102cee905d0145ac4221607b1
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:29:29 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
@@ -1,6 +1,7 @@
 package com.dragontalker.java1;
 
 import java.net.URL;
+import java.security.Provider;
 
 public class ClassLoaderTest1 {
     public static void main(String[] args) {
@@ -12,6 +13,11 @@ public class ClassLoaderTest1 {
         }
 
         // 从上面的路径中随意选择一个类, 来看看他的类加载器是什么
+        // null: 引导类加载器
+        ClassLoader classLoader = Provider.class.getClassLoader();
+        System.out.println(classLoader); // null
+
+
         System.out.println("***********扩展类加载器***********");
         String extDirs = System.getProperty("java.ext.dirs");
         for (String path : extDirs.split(";")) {
