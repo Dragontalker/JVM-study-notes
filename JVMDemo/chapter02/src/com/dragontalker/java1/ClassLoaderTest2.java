8dff9fa68ef071c890590d5c6c5d978b74cdb03c
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:50:28 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest2.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest2.java
@@ -12,6 +12,10 @@ public class ClassLoaderTest2 {
             // 2. 获取当前线程上下文的ClassLoader
             ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
             System.out.println(contextClassLoader); // sun.misc.Launcher$AppClassLoader@18b4aac2
+
+            // 3. 获取系统的ClassLoader
+            ClassLoader classLoader1 = ClassLoader.getSystemClassLoader().getParent();
+            System.out.println(classLoader1); // sun.misc.Launcher$ExtClassLoader@1540e19d
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
