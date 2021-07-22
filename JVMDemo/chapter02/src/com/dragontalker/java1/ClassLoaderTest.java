c6064606a7799a956f5ca295fffefff68fa83561
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:10:41 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest.java
@@ -6,5 +6,9 @@ public class ClassLoaderTest {
         // 获取系统类加载器
         ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
         System.out.println(systemClassLoader); // sun.misc.Launcher$AppClassLoader@18b4aac2
+
+        // 获取其上层: 扩展类加载器
+        ClassLoader extClassLoader = systemClassLoader.getParent();
+        System.out.println(extClassLoader); // sun.misc.Launcher$ExtClassLoader@1540e19d
     }
 }
