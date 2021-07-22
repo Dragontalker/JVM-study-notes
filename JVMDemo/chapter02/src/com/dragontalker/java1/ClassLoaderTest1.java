99ee1206214e6046d0ccc149db33b1db410caa7f
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:25:54 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest1.java
@@ -9,5 +9,12 @@ public class ClassLoaderTest1 {
         for (URL element : urls) {
             System.out.println(element.toExternalForm());
         }
+
+        // 从上面的路径中随意选择一个类, 来看看他的类加载器是什么
+        System.out.println("***********扩展类加载器***********");
+        String extDirs = System.getProperty("java.ext.dirs");
+        for (String path : extDirs.split(";")) {
+            System.out.println(path);
+        }
     }
 }
