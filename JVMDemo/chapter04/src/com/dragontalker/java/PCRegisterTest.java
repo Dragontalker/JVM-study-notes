f2e7d392df2ced5fdbb34b891a0ecde61350f26e
Richard <richard.yang.tong@gmail.com>
2021-07-22 15:37:47 -0400

    Added notes.

--- JVMDemo/chapter04/src/com/dragontalker/java/PCRegisterTest.java
+++ JVMDemo/chapter04/src/com/dragontalker/java/PCRegisterTest.java
@@ -1,4 +1,10 @@
 package com.dragontalker.java;
 
 public class PCRegisterTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+    }
 }
