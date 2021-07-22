a54bde2f7cc703d905a464248214a7aea6af5f63
Richard <richard.yang.tong@gmail.com>
2021-07-22 09:47:59 -0400

    Added notes.

--- JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest2.java
+++ JVMDemo/chapter02/src/com/dragontalker/java1/ClassLoaderTest2.java
@@ -1,4 +1,14 @@
 package com.dragontalker.java1;
 
 public class ClassLoaderTest2 {
+
+    public static void main(String[] args) {
+
+        try {
+            ClassLoader classLoader = Class.forName("java.lang.String").getClassLoader();
+            System.out.println(classLoader); // null
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
