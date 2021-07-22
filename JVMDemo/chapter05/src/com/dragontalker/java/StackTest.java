12ab5f927f91b1c162e99792c2d0c0176c80d6b7
Richard <richard.yang.tong@gmail.com>
2021-07-22 16:12:04 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java/StackTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java/StackTest.java
@@ -1,4 +1,16 @@
 package com.dragontalker.java;
 
 public class StackTest {
+
+    public void methodA() {
+        int i = 10;
+        int j = 20;
+
+        methodB();
+    }
+
+    public void methodB() {
+        int k = 30;
+        int m = 40;
+    }
 }
