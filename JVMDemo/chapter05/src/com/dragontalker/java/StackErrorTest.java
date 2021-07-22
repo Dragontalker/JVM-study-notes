ee25f1e60a3a893372d1c0c25d4a294e1a8e455b
Richard <richard.yang.tong@gmail.com>
2021-07-22 16:44:24 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java/StackErrorTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java/StackErrorTest.java
@@ -2,9 +2,17 @@ package com.dragontalker.java;
 
 /**
  * 演示栈中的异常
+ *
+ * 默认情况下: count: 11422
+ * 设置栈大小: Xss256k : count: 2459
  */
 public class StackErrorTest {
+
+    private static int count = 1;
+
     public static void main(String[] args) {
+        System.out.println(count);
+        count++;
         main(args);
     }
 }
