b515f0db70391bb2a20ccdaf4d2eed1b81a60bcc
Richard <richard.yang.tong@gmail.com>
2021-07-25 19:43:16 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
@@ -3,6 +3,7 @@ package com.dragontalker.java1;
 public class OperandStackTest {
 
     public void testAddOperation() {
+        // byte, short, char, boolean: 都以int型来保存
         byte i = 15;
         int j = 8;
         int k = i + j;
