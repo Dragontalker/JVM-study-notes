63917e25972eab1ca57075cae1ef6a54cf79ec24
Richard <richard.yang.tong@gmail.com>
2021-07-25 19:41:08 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
@@ -1,4 +1,10 @@
 package com.dragontalker.java1;
 
 public class OperandStackTest {
+
+    public void testAddOperation() {
+        byte i = 15;
+        int j = 8;
+        int k = i + j;
+    }
 }
