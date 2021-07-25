45dd91b33e11b426ea1a37afa71742115f8285cb
Richard <richard.yang.tong@gmail.com>
2021-07-25 19:50:54 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
@@ -8,4 +8,16 @@ public class OperandStackTest {
         int j = 8;
         int k = i + j;
     }
+
+    public int getSum() {
+        int m = 10;
+        int n = 20;
+        int k = m + n;
+        return k;
+    }
+
+    public void testGetSum() {
+        int i = getSum();
+        int j = 10;
+    }
 }
