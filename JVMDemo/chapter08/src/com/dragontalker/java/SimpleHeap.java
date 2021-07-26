eda8f2413946ae38ed9672259264cee8c7fc2a62
Richard <richard.yang.tong@gmail.com>
2021-07-26 19:13:02 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java/SimpleHeap.java
+++ JVMDemo/chapter08/src/com/dragontalker/java/SimpleHeap.java
@@ -1,4 +1,22 @@
 package com.dragontalker.java;
 
 public class SimpleHeap {
+    private int id;
+
+    public SimpleHeap(int id) {
+        this.id = id;
+    }
+
+    public void show() {
+        System.out.println("My ID is " + id);
+    }
+
+    public static void main(String[] args) {
+        SimpleHeap s1 = new SimpleHeap(1);
+        SimpleHeap s2 = new SimpleHeap(2);
+
+        int[] arr = new int[10];
+
+        Object[] arr1 = new Object[10];
+    }
 }
