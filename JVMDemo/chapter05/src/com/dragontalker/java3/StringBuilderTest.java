4a6592f32ccd6a70b4184307f5f7b4e0a3525ca4
Richard <richard.yang.tong@gmail.com>
2021-07-26 12:20:34 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java3/StringBuilderTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java3/StringBuilderTest.java
@@ -9,6 +9,8 @@ package com.dragontalker.java3;
  *  - 如果有多个线程操作此数据, 则此数据是共享数据, 如果不考虑同步机制的话, 会存在线程安全问题
  */
 public class StringBuilderTest {
+
+    // s1的声明方式是线程安全的
     public static void method1() {
         // StringBuilder: 线程不安全
         StringBuilder s1 = new StringBuilder();
