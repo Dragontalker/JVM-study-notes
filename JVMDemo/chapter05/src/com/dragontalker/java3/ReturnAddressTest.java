478253803470b6a01f56e99cdfe4c58a50000c94
Richard <richard.yang.tong@gmail.com>
2021-07-26 11:48:33 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java3/ReturnAddressTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java3/ReturnAddressTest.java
@@ -1,5 +1,6 @@
 package com.dragontalker.java3;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
 
@@ -59,5 +60,16 @@ public class ReturnAddressTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void method1() throws IOException {
+        FileReader fis = new FileReader("atguigu.txt");
+        char[] cBuffer = new char[1024];
+        int len;
+        while((len = fis.read(cBuffer)) != -1) {
+            String str = new String(cBuffer, 0, len);
+            System.out.println(str);
+        }
+        fis.close();
     }
 }
