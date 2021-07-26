baab906be17666b99eeb6faa9a738a412dd77abc
Richard <richard.yang.tong@gmail.com>
2021-07-26 10:21:16 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java2/Son.java
+++ JVMDemo/chapter05/src/com/dragontalker/java2/Son.java
@@ -1,4 +1,22 @@
 package com.dragontalker.java2;
 
+class Father {
+    public Father() {
+        System.out.println("father的构造器");
+    }
+
+    public static void showStatic(String str) {
+        System.out.println("father " + str);
+    }
+
+    public final void showFinal() {
+        System.out.println("father show final");
+    }
+
+    public void showCommon() {
+        System.out.println("father 普通方法");
+    }
+}
+
 public class Son {
 }
