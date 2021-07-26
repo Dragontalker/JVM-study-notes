3945da4aa143c9c3fd975e72c785a79c76c912fe
Richard <richard.yang.tong@gmail.com>
2021-07-26 11:07:10 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java2/Lambda.java
+++ JVMDemo/chapter05/src/com/dragontalker/java2/Lambda.java
@@ -1,4 +1,19 @@
 package com.dragontalker.java2;
 
+interface Func {
+    boolean func(String str);
+}
+
 public class Lambda {
+    public void lambda(Func func) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        Lambda lambda = new Lambda();
+        Func func = s -> {
+            return true;
+        };
+        lambda.lambda(func);
+    }
 }
