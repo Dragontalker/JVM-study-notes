680fef435e8c5790d9e7c32f932480cddd42df5c
Richard <richard.yang.tong@gmail.com>
2021-07-26 11:45:13 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java3/ReturnAddressTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java3/ReturnAddressTest.java
@@ -1,4 +1,31 @@
 package com.dragontalker.java3;
 
 public class ReturnAddressTest {
+    public boolean methodBoolean() {
+        return false;
+    }
+
+    public byte methodByte() {
+        return 0;
+    }
+
+    public short methodShort() {
+        return 0;
+    }
+
+    public char methodChar() {
+        return 'a';
+    }
+
+    public int methodInt() {
+        return 0;
+    }
+
+    public long methodLong() {
+        return 0L;
+    }
+
+    public float methodFloat() {
+        return 0.0f;
+    }
 }
