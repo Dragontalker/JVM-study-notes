470e5aeb76f6a5fd645efc9e27e592af68a74ea3
Richard <richard.yang.tong@gmail.com>
2021-07-26 12:16:07 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java3/StringBuilderTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java3/StringBuilderTest.java
@@ -1,4 +1,15 @@
 package com.dragontalker.java3;
 
+/**
+ * 面试题:
+ * 方法中定义的局部变量是否线程安全? 具体情况具体分析
+ */
 public class StringBuilderTest {
+    public static void method1() {
+        // StringBuilder: 线程不安全
+        StringBuilder s1 = new StringBuilder();
+        s1.append("a");
+        s1.append("b");
+        // ..
+    }
 }
