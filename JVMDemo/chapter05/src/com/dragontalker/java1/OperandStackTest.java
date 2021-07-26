0cdf3136bec4d7ec4070288e3159b56c54f91671
Richard <richard.yang.tong@gmail.com>
2021-07-25 20:01:36 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
+++ JVMDemo/chapter05/src/com/dragontalker/java1/OperandStackTest.java
@@ -39,5 +39,16 @@ public class OperandStackTest {
 
         int i5 = 10;
         int i6 = ++i5;
+
+        // 第三类问题:
+        int i7 = 10;
+        i7 = i7++;
+
+        int i8 = 10;
+        i8 = i8++;
+
+        // 第四类问题:
+        int i9 = 10;
+        int i10 = i9++ + ++i9;
     }
 }
