821cedf3ab8a44d4915e0e77f6791cbcaec6a4ec
Richard <richard.yang.tong@gmail.com>
2021-07-26 16:27:55 -0400

    Added notes.

---
+++ JVMDemo/chapter08/src/com/dragontalker/java/HeapDemo1.java
@@ -0,0 +1,15 @@
+package com.dragontalker.java;
+
+public class HeapDemo1 {
+    public static void main(String[] args) {
+        System.out.println("start...");
+
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end...");
+    }
+}
