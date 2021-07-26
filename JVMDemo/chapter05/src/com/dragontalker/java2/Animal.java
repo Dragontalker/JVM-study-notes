3446e6e3c2ea1635f039e0acd2c098dd624b2234
Richard <richard.yang.tong@gmail.com>
2021-07-26 09:51:57 -0400

    Added notes.

--- JVMDemo/chapter05/src/com/dragontalker/java2/Animal.java
+++ JVMDemo/chapter05/src/com/dragontalker/java2/Animal.java
@@ -4,4 +4,24 @@ package com.dragontalker.java2;
  * 说明早期绑定和晚期绑定的例子
  */
 class Animal {
+    public void eat() {
+        System.out.println("动物进食");
+    }
+}
+
+interface Huntable {
+    void hunt();
+}
+
+class Dog extends Animal implements Huntable {
+
+    @Override
+    public void eat() {
+        System.out.println("狗吃骨头");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("捕食耗子, 多管闲事");
+    }
 }
