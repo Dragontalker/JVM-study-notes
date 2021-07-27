611fce77d36430300cdddb3ec5079429a22fda5e
Richard <richard.yang.tong@gmail.com>
2021-07-26 20:46:24 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/EdenSurvivorTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/EdenSurvivorTest.java
@@ -1,5 +1,10 @@
 package com.dragontalker.java1;
 
+/**
+ * -Xms600m -Xmx600m -XX:+PrintGCDetails
+ *
+ * -NewRatio: 设置新生代与老年代的比例, 默认是值是2, 即1:2
+ */
 public class EdenSurvivorTest {
     public static void main(String[] args) {
         System.out.println("我只是来打个酱油~");
