95a96ff349b75c60d6c24251ad9d8cd2c163efe3
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:46:01 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
@@ -1,4 +1,18 @@
 package com.dragontalker.java1;
 
+/**
+ * 测试堆空间常用的JVM参数
+ * -XX:+PrintFlagsInitial:
+ * -XX:+PrintFlagsFinal:
+ * -Xms:
+ * -Xmx:
+ * -XX:NewRatio:
+ * -XX:SurvivorRatio:
+ * -XX:MaxTenuringThreshold:
+ * -XX:+PrintGCDetails:
+ */
 public class HeapArgsTest {
+    public static void main(String[] args) {
+
+    }
 }
