f02fa8795981e5dd4e0ba924c572d39d3a288960
Richard <richard.yang.tong@gmail.com>
2021-07-27 09:53:48 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/GCTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/GCTest.java
@@ -1,4 +1,22 @@
 package com.dragontalker.java1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GCTest {
+    public static void main(String[] args) {
+        int i = 0;
+        try {
+            List<String> list = new ArrayList<>();
+            String a = "atguigu.com";
+            while(true) {
+                list.add(a);
+                a = a + a;
+                i++;
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.out.println("遍历次数为: " + i);
+        }
+    }
 }
