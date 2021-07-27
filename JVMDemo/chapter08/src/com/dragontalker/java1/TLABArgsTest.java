2a80619d3be2a07e260ca01d7c804d83a12e1c32
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:35:26 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/TLABArgsTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/TLABArgsTest.java
@@ -1,5 +1,9 @@
 package com.dragontalker.java1;
 
+/**
+ * 测试-XX:UserTLAB参数是否开启的情况
+ *
+ */
 public class TLABArgsTest {
     public static void main(String[] args) {
         System.out.println("我只是来打个酱油~");
