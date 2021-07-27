8648c6a949a347479e388dfc4dcd970ae78673eb
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:34:46 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/TLABArgsTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/TLABArgsTest.java
@@ -1,4 +1,12 @@
 package com.dragontalker.java1;
 
 public class TLABArgsTest {
+    public static void main(String[] args) {
+        System.out.println("我只是来打个酱油~");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
