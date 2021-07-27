b448770d5ca19b672d1532a165c54ce596593dfb
Richard <richard.yang.tong@gmail.com>
2021-07-26 20:55:14 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/EdenSurvivorTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/EdenSurvivorTest.java
@@ -4,7 +4,7 @@ package com.dragontalker.java1;
  * -Xms600m -Xmx600m -XX:+PrintGCDetails
  *
  * -NewRatio: 设置新生代与老年代的比例, 默认是值是2, 即1:2
- *
+ * -XX:SurvivorRatio: 设置新生代中Eden区与Survivor区的比例
  * -XX:-UseAdaptiveSizePolicy: 关闭自适应的内存分配策略(暂时用不到)
  */
 public class EdenSurvivorTest {
