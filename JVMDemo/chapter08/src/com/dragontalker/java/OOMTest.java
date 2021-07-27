60e087f6fe05c4a42b3ab37fd311c1604d7becd2
Richard <richard.yang.tong@gmail.com>
2021-07-26 20:22:11 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java/OOMTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java/OOMTest.java
@@ -2,3 +2,11 @@ package com.dragontalker.java;
 
 public class OOMTest {
 }
+
+class Picture {
+    private byte[] pixels;
+
+    public Picture(int length) {
+        this.pixels = new byte[length];
+    }
+}
