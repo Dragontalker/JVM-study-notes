10243a74af2576065dfde577d6a596a578d66b3e
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:52:44 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
@@ -2,14 +2,15 @@ package com.dragontalker.java1;
 
 /**
  * 测试堆空间常用的JVM参数
- * -XX:+PrintFlagsInitial:
- * -XX:+PrintFlagsFinal:
- * -Xms:
- * -Xmx:
- * -XX:NewRatio:
- * -XX:SurvivorRatio:
- * -XX:MaxTenuringThreshold:
- * -XX:+PrintGCDetails:
+ * -XX:+PrintFlagsInitial: 查看所有的参数的默认初始值
+ * -XX:+PrintFlagsFinal: 查看所有的参数的最终值(可能会存在修改, 不再是初始值)
+ * -Xms: 初始堆空间内存(默认为物理内存的1/64)
+ * -Xmx: 最大堆空间内存(默认为物理内存的1/4)
+ * -XX:NewRatio: 设置新生代的大小(初始值及最大值)
+ * -XX:SurvivorRatio: 设置新生代中Eden和S0/S1空间的比例
+ * -XX:MaxTenuringThreshold: 设置新生代垃圾的最大年龄
+ * -XX:+PrintGCDetails: 输出详细的GC处理日志
+ *
  */
 public class HeapArgsTest {
     public static void main(String[] args) {
