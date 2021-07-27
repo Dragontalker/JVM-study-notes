a068b3d94582b03a1ec78f774142e00b0efa2385
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:56:47 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/HeapArgsTest.java
@@ -4,6 +4,9 @@ package com.dragontalker.java1;
  * 测试堆空间常用的JVM参数
  * -XX:+PrintFlagsInitial: 查看所有的参数的默认初始值
  * -XX:+PrintFlagsFinal: 查看所有的参数的最终值(可能会存在修改, 不再是初始值)
+ *      具体查看某个参数的指令:
+ *          1. jps: 查看当前运行的进程
+ *          2. jinfo -flag SurvivorRatio 进程id
  * -Xms: 初始堆空间内存(默认为物理内存的1/64)
  * -Xmx: 最大堆空间内存(默认为物理内存的1/4)
  * -XX:NewRatio: 设置新生代的大小(初始值及最大值)
