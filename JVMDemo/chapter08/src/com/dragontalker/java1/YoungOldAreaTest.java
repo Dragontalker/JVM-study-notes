7fcdbd9987b32b2ca116da109ce23815fafd3ce5
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:27:51 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/YoungOldAreaTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/YoungOldAreaTest.java
@@ -3,9 +3,12 @@ package com.dragontalker.java1;
 /**
  * 测试: 大对象直接进入老年代
  * -Xms60m -Xmx60m -XX:NewRatio=2 -XX:SurvivorRatio=8 -XX:PrintGCDetails
+ *
+ * Eden: 16m, S0: 2m, S1: 2m, Old: 40m
  */
 public class YoungOldAreaTest {
     public static void main(String[] args) {
-        byte[] buffer = new byte[1024 * 1024 * 20]; // 20m
+        // 20m, 大于Eden的16m, 直接进入老年代
+        byte[] buffer = new byte[1024 * 1024 * 20];
     }
 }
