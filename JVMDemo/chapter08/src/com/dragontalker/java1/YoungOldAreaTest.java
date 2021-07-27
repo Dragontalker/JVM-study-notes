24915744fa697fe4ef2da93f9b9fd6e585b9d09f
Richard <richard.yang.tong@gmail.com>
2021-07-27 10:24:52 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java1/YoungOldAreaTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java1/YoungOldAreaTest.java
@@ -1,4 +1,7 @@
 package com.dragontalker.java1;
 
 public class YoungOldAreaTest {
+    public static void main(String[] args) {
+        byte[] buffer = new byte[1024 * 1024 * 20]; // 20m
+    }
 }
