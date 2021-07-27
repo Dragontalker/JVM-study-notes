52a06d95ddc6cfde0078cd85a6a119de867d39e7
Richard <richard.yang.tong@gmail.com>
2021-07-26 20:24:08 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java/OOMTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java/OOMTest.java
@@ -1,6 +1,20 @@
 package com.dragontalker.java;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class OOMTest {
+    public static void main(String[] args) {
+        ArrayList<Picture> list = new ArrayList<>();
+        while(true) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(new Picture(new Random().nextInt(1024 * 1024)));
+        }
+    }
 }
 
 class Picture {
