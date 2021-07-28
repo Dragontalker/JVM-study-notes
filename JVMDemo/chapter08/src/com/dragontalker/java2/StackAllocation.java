12fa820eb090375f2530d0a7310d8f00f8d7e56b
Richard <richard.yang.tong@gmail.com>
2021-07-28 16:09:53 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java2/StackAllocation.java
+++ JVMDemo/chapter08/src/com/dragontalker/java2/StackAllocation.java
@@ -1,4 +1,22 @@
 package com.dragontalker.java2;
 
 public class StackAllocation {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 10000000; i++) {
+            alloc();
+        }
+
+        // 查看执行时间
+        long end = System.currentTimeMillis();
+        System.out.println("花费的时间为: " + (end - start) + "ms");
+
+        // 为了方便查看堆内存中对象个数, 线程sleep
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
