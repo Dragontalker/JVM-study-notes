d1a84c98ecafc8004d662131f5aac028188800db
Richard <richard.yang.tong@gmail.com>
2021-07-28 16:32:12 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java2/ScalarReplace.java
+++ JVMDemo/chapter08/src/com/dragontalker/java2/ScalarReplace.java
@@ -2,3 +2,15 @@ package com.dragontalker.java2;
 
 public class ScalarReplace {
 }
+
+/*
+class Customer {
+    String name;
+    int id;
+    Account acct;
+}
+
+class Account {
+    double balance;
+}
+ */
