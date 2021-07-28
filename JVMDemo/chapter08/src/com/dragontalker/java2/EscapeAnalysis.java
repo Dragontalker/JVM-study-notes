bbe03651f3f47df94c20dc1c6ff46186e496810e
Richard <richard.yang.tong@gmail.com>
2021-07-28 11:00:24 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java2/EscapeAnalysis.java
+++ JVMDemo/chapter08/src/com/dragontalker/java2/EscapeAnalysis.java
@@ -1,5 +1,10 @@
 package com.dragontalker.java2;
 
+/**
+ * 逃逸分析
+ *
+ * 如何快速的判断是否发生了逃逸分析, 大家就看new的对象是否有可能在方法外被调用
+ */
 public class EscapeAnalysis {
 
     public EscapeAnalysis obj;
@@ -17,4 +22,8 @@ public class EscapeAnalysis {
     public void setObj() {
         this.obj = new EscapeAnalysis();
     }
+
+    /*
+    对象的作用域仅在
+     */
 }
