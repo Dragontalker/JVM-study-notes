77ddb08f95b0c6231775725effac612acae08c88
Richard <richard.yang.tong@gmail.com>
2021-07-28 10:58:32 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java2/EscapeAnalysis.java
+++ JVMDemo/chapter08/src/com/dragontalker/java2/EscapeAnalysis.java
@@ -1,4 +1,13 @@
 package com.dragontalker.java2;
 
 public class EscapeAnalysis {
+
+    public EscapeAnalysis obj;
+
+    /*
+    方法返回EscapeAnalysis对象, 发生逃逸
+     */
+    public EscapeAnalysis getInstance() {
+        return obj == null? new EscapeAnalysis() : obj;
+    }
 }
