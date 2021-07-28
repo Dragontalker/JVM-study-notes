9b3a45b26aabc6a363e16a705d0e6fbd5d2f7c50
Richard <richard.yang.tong@gmail.com>
2021-07-28 17:15:45 -0400

    Added notes.

--- JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaDemo.java
+++ JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaDemo.java
@@ -1,4 +1,13 @@
 package com.dragontalker.java;
 
 public class MethodAreaDemo {
+    public static void main(String[] args) {
+        System.out.println("start...");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end...");
+    }
 }
