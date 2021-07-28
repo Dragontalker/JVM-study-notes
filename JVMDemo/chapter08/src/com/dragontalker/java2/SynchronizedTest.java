955d95d0ea0205ff1618ff45fc480545233802c3
Richard <richard.yang.tong@gmail.com>
2021-07-28 16:21:19 -0400

    Added notes.

--- JVMDemo/chapter08/src/com/dragontalker/java2/SynchronizedTest.java
+++ JVMDemo/chapter08/src/com/dragontalker/java2/SynchronizedTest.java
@@ -1,4 +1,10 @@
 package com.dragontalker.java2;
 
 public class SynchronizedTest {
+    public void f() {
+        Object hollis = new Object();
+        synchronized (hollis) {
+            System.out.println(hollis);
+        }
+    }
 }
