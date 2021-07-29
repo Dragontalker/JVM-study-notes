91ab5cc0c1173da407fc4dc3cdf54a5d63ff1b49
Richard <richard.yang.tong@gmail.com>
2021-07-29 08:58:11 -0400

    Added notes.

--- JVMDemo/chapter09/src/com/dragontalker/java/MethodInnerStrucTest.java
+++ JVMDemo/chapter09/src/com/dragontalker/java/MethodInnerStrucTest.java
@@ -1,4 +1,20 @@
 package com.dragontalker.java;
 
-public class MethodInnerStrucTest {
+import java.io.Serializable;
+
+public class MethodInnerStrucTest  extends Object implements Comparable<String>, Serializable {
+    // 属性
+    public int num = 10;
+    private static String str = "测试方法的内部结构";
+    // 构造器
+    // 方法
+    public void test1() {
+        int count = 20;
+        System.out.println("count = " + count);
+    }
+
+    @Override
+    public int compareTo(String o) {
+        return 0;
+    }
 }
