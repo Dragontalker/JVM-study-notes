9fe444fd0478b1f067c890b63dd6c250dfff7689
Richard <richard.yang.tong@gmail.com>
2021-07-28 22:35:02 -0400

    Added notes.

--- JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaDemo.java
+++ JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaDemo.java
@@ -2,6 +2,11 @@ package com.dragontalker.java;
 
 /**
  * 设置堆空间的大小: -Xms600m -Xmx600m
+ *
+ * 设置方法区大小参数的默认值
+ *
+ * JDK8及以后:
+ * -XX:MetaspaceSize=100m
  */
 public class MethodAreaDemo {
     public static void main(String[] args) {
