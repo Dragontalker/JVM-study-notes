cdc4c35946925f807182799799c634ee230b6e04
Richard <richard.yang.tong@gmail.com>
2021-07-29 09:33:56 -0400

    Added notes.

--- JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaTest.java
+++ JVMDemo/chapter09/src/com/dragontalker/java/MethodAreaTest.java
@@ -1,4 +1,18 @@
 package com.dragontalker.java;
 
 public class MethodAreaTest {
+    public static void main(String[] args) {
+        Order order = null;
+        order.hello();
+        System.out.println(order.count);
+    }
+}
+
+class Order {
+    public static int count = 1;
+    public static final int number = 2;
+
+    public static void hello() {
+        System.out.println("hello");
+    }
 }
